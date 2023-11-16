f4e5f20d47989ae8b3eb5410a515b0c01875df81
BhuvanaShanker <bhuvanstr83@gmail.com>
2023-11-16 21:06:36 +0530

    login page interface

--- src/main/java/com/Limeroad_Bug_Finders/pom/LoginPage.java
+++ src/main/java/com/Limeroad_Bug_Finders/pom/LoginPage.java
@@ -1,5 +1,12 @@
 package com.Limeroad_Bug_Finders.pom;
 
 public interface LoginPage {
-
+	
+	String choose_men = "(//a[@target='_blank'])[8]";
+	String sub_men_product = "(//span[@title='translation missing: en.polo_t-shirts'])[1]";
+	String brand = "//div[@data-a='brandid']";
+	String bee = "//label[@for='fltr-79731']";
+	String choose_dress = "//img[@alt='Product image']";
+	String choose_size = "//div[@data-a='size']";
+	String Addtocart = "//div[@data-pgn='Add to Cart']";
 }
